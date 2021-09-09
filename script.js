0ba860a2c99394fe42ab657194181e4598d09eb2
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-09 17:09:27 +0000

    JavaScript array.

---
+++ script.js
@@ -0,0 +1,9 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
+let countryName = ["Canada", "Africa", "Argentina"];
+countryName.toString;
+
+console.log(countryName.length);
