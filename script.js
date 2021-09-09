d67a1a728501545c5504cc697d00cbe3c6679f55
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-09 17:17:55 +0000

    Writing javascript code array

--- script.js
+++ script.js
@@ -3,7 +3,10 @@
  * @author joey@tech-center.com (Joey Cadieux)
  */
 
-let countryName = ["Canada", "Africa", "Argentina"];
+
+let countryName = ["Canada", "United State", "Mexico"];
 countryName.toString;
+
+document.getElementById("countryName").innerHTML = countryName;
 
 console.log(countryName.length);
