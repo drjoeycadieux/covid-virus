97c1bdddd86a801615d75bf88e5dabed3ddc9ac7
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-09 20:55:01 +0000

    ADDING JAVASCRIPT OBJECT

---
+++ module/index.js
@@ -0,0 +1,22 @@
+/*
+ * @fileoverview Utilities for manipulating JavaScript code.
+ * @author joey@tech-center.com (Joey Cadieux)
+ */
+
+document.getElementById("cases").innerHTML = cases;
+document.getElementById("newCases").innerHTML = newCases;
+document.getElementById("death").innerHTML = death;
+document.getElementById("newDeath").innerHTML = newDeath;
+
+
+
+
+const dailyOverview = [
+	{
+		date: '2021-09-09',
+		cases: 395155,
+		newCases: 703,
+		death: 11297,
+		newDeath: 1,
+	},
+];
