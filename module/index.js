b9d893b3eefcfa1a57cd105249b046bdf635d623
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-10 15:11:54 +0000

    Adding covid-19 data for Ontario

--- module/index.js
+++ module/index.js
@@ -11,10 +11,11 @@ const dailyOverview = [{
 	newDeath: 1
 }];
 
-
 // dailyOverview[0] gets the first object, wrapped in braces {}.
 // dailyOverview[1] would get the second object in braces.
 // adding '.cases', '.newCases', etc. gets the property you want.
+
+// dailyOverview ->
 
 document.getElementById("cases").innerHTML = dailyOverview[0].cases;
 document.getElementById("newCases").innerHTML = dailyOverview[0].newCases;
