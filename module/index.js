09049d5365def9cc9394aadaaa77691053cfe236
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-10 13:14:49 +0000

    Fix issues in the code!

--- module/index.js
+++ module/index.js
@@ -3,20 +3,26 @@
  * @author joey@tech-center.com (Joey Cadieux)
  */
 
-document.getElementById("cases").innerHTML = cases;
-document.getElementById("newCases").innerHTML = newCases;
-document.getElementById("death").innerHTML = death;
-document.getElementById("newDeath").innerHTML = newDeath;
+const dailyOverview = [{
+	date: '2021-09-09',
+	cases: 395155,
+	newCases: 703,
+	death: 11297,
+	newDeath: 1
+}];
 
 
+// dailyOverview[0] gets the first object, wrapped in braces {}.
+// dailyOverview[1] would get the second object in braces.
+// adding '.cases', '.newCases', etc. gets the property you want.
+
+document.getElementById("cases").innerHTML = dailyOverview[0].cases;
+document.getElementById("newCases").innerHTML = dailyOverview[0].newCases;
+document.getElementById("death").innerHTML = dailyOverview[0].death;
+document.getElementById("newDeath").innerHTML = dailyOverview[0].newDeath;
 
 
-const dailyOverview = [
-	{
-		date: '2021-09-09',
-		cases: 395155,
-		newCases: 703,
-		death: 11297,
-		newDeath: 1,
-	},
-];
+document.getElementById("cases").innerHTML = dailyOverview[1].cases;
+document.getElementById("newCases").innerHTML = dailyOverview[1].newCases;
+document.getElementById("death").innerHTML = dailyOverview[1].death;
+document.getElementById("newDeath").innerHTML = dailyOverview[1].newDeath;
