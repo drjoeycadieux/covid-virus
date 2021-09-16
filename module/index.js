f8568a4bc7861786ede13f399e765cf134fd1a61
Joey Cadieux <instructor.joeycadieux@gmail.com>
2021-09-16 13:31:43 +0000

    Auth Nuul:

--- module/index.js
+++ module/index.js
@@ -8,42 +8,42 @@
 // adding '.cases', '.newCases', etc. gets the property you want.
 
 const dailyOverview = [
-  {
-    date: "2021-09-15",
-    cases: 399058, // Done
-    newCases: 879,
-    death: 11312, // Done
-    newDeath: 7, // Done
-  }
+	{
+		date: "2021-09-15",
+		cases: 399058, // Done
+		newCases: 879,
+		death: 11312, // Done
+		newDeath: 7, // Done
+	}
 ];
 
 const dailyMontreal = [
-  {
-    date: "2021-09-15",
-    positiveCases: 268,
-    totalDeath: 4778
-  }
+	{
+		date: "2021-09-15",
+		positiveCases: 268,
+		totalDeath: 4778
+	}
 ];
 
 const dailyEverything = [
-  {
-    date: "2021-09-15", // never change the date
-    hospitalizations: 230, // Done
-    vaccination: 12570901,
-    totalRecovered: 381505, // Done
-    totalVaccination: 771,
-    activeCases: 6012
-  }
+	{
+		date: "2021-09-15", // never change the date
+		hospitalizations: 230, // Done
+		vaccination: 12570901,
+		totalRecovered: 381505, // Done
+		totalVaccination: 771,
+		activeCases: 6012
+	}
 ];
 
 const dailyVirus = [
-  {
-    date: "2021-09-15",
-    cases: 10155,
-    newCases: 10290,
-    death: 9579,
-    newDeath: 39830
-  }
+	{
+		date: "2021-09-15",
+		cases: 10155,
+		newCases: 10290,
+		death: 9579,
+		newDeath: 39830
+	}
 ];
 
 // dailyVirus ->
@@ -56,24 +56,24 @@ document.getElementById("newDeath1").innerHTML = dailyVirus[0].newDeath;
 // dailyEverything ->
 
 document.getElementById("activeCases").innerHTML =
-  dailyEverything[0].activeCases;
+	dailyEverything[0].activeCases;
 
 document.getElementById("totalDeath").innerHTML = dailyMontreal[0].totalDeath;
 
 document.getElementById("totalRecovered").innerHTML =
-  dailyEverything[0].totalRecovered;
+	dailyEverything[0].totalRecovered;
 
 document.getElementById("vaccination").innerHTML =
-  dailyEverything[0].vaccination;
+	dailyEverything[0].vaccination;
 
 document.getElementById("hospitalizations").innerHTML =
-  dailyEverything[0].hospitalizations;
+	dailyEverything[0].hospitalizations;
 
 document.getElementById("totalVaccination").innerHTML =
-  dailyEverything[0].totalVaccination;
+	dailyEverything[0].totalVaccination;
 
 document.getElementById("positiveCases").innerHTML =
-  dailyMontreal[0].positiveCases;
+	dailyMontreal[0].positiveCases;
 
 // dailyOverview ->
 
